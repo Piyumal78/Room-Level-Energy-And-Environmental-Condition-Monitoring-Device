d4e812f205b56ce15501d1b4366c0ef9e10df1d7
Piyumal78 <tharindupiyumal281@gmail.com>
2025-01-12 23:09:41 +0530

    Delete my_crative_project directory

--- my_crative_project/RoomData/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RoomDataList, RoomDataDetail
-
-urlpatterns = [
-    path('room-data/', RoomDataList.as_view(), name='room-data-list'),
-    path('room-data/<int:pk>/', RoomDataDetail.as_view(), name='room-data-detail'),
-]
